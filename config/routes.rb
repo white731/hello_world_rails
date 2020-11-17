5f46ce39e609ed3b4dbfec91e1c1e1161de79d45
Landon Whitesides <whitesideslandon@gmail.com>
2020-11-16 17:31:14 -0700

    client can delete

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
-  # get 'pages/index'
-  # get 'pages/show'
-  # get 'pages/new'
+  # root 'static_pages#home'
+  root 'static_pages#home'
 
+
+  #custom route
+  get '/about', to: 'static_pages#about'
+
+  
+
+  #resources generates crud action routes in this case from pages
   resources :pages
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
